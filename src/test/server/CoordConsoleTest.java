820838a249bbd3fd17f6a6f47d9eedc3eb7e65d8
mark.german <mark.german@ucalgary.ca>
2020-03-03 00:07:14 -0700

    Attempting to fix maven build

--- src/test/server/CoordConsoleTest.java
+++ src/test/server/CoordConsoleTest.java
@@ -1,4 +1,4 @@
-package test;
+package server;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
 
 import server.CoordConsole;
 
