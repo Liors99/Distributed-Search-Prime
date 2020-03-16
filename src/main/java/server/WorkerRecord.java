f9c17bde7b0d9bb9e008a306eb4d74c87d26ce7e
Nunuvin <zloyzig@yandex.ru>
2020-03-16 17:46:38 -0600

    Task scheduler base; todo: how result works. Score det. additional ds?

--- src/main/java/server/WorkerRecord.java
+++ src/main/java/server/WorkerRecord.java
@@ -1,10 +1,13 @@
 package server;
 
+import java.math.BigInteger;
 import java.sql.Timestamp;
 
 public class WorkerRecord extends Record{
     private int WID;
     private double score;
+    private Timestamp startedWork;
+    private BigInteger[] workrange;
     static final double DEFAULTSCORE=100;
 
     WorkerRecord(){super();}
@@ -16,6 +19,13 @@ public class WorkerRecord extends Record{
         this.setWID(WID);
     }
 
+    /**
+     * change score based on time spent on previous task
+     * @param delta
+     */
+    private void deriveScore(long delta){
+
+    }
 
     public int getWID() {
         return WID;
@@ -31,5 +41,30 @@ public class WorkerRecord extends Record{
 
     public void setScore(double score) {
         this.score = score;
+    }
+
+    public void stopWork(){
+        Timestamp stop = new Timestamp(System.currentTimeMillis());
+        long delta = stop.getTime() - startedWork.getTime();
+        deriveScore(delta);
+    }
+
+    public Timestamp getStartedWork() {
+        return startedWork;
+    }
+
+    public void setStartedWork(Timestamp startedWork) {
+        this.startedWork = startedWork;
+    }
+    public void startWork(){
+        startedWork = new Timestamp(System.currentTimeMillis());
+    }
+
+    public BigInteger[] getWorkrange() {
+        return workrange;
+    }
+
+    public void setWorkrange(BigInteger[] workrange) {
+        this.workrange = workrange;
     }
 }
