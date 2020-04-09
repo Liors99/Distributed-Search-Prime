2753cc12456ff2a924da7500de1816cd5cd983aa
Wilfredo Jr. Pasadilla <wilfredojp3@gmail.com>
2020-04-09 16:20:44 -0600

    Layed Out Plan

--- src/main/java/server/ElectionProtocol.java
+++ src/main/java/server/ElectionProtocol.java
@@ -7,7 +7,7 @@ public class ElectionProtocol {
 	/**
 	 * Randomly generate a number to simulate the roll of a die. 
 	 *
-	 * @return      the randomly determined number
+	 * @return	the randomly determined number
 	 */
 	public static double roll() {
 		double roll = 0; 
@@ -18,9 +18,9 @@ public class ElectionProtocol {
 	/**
 	 * Comparison operator for dice rolls 
 	 *
-	 * @param  roll1 the random roll of object 1 
-	 * @param  roll2 the random roll of object 2 
-	 * @return      true if roll1 higher than roll2, false otherwise. 
+	 * @param	roll1 the random roll of object 1 
+	 * @param  	roll2 the random roll of object 2 
+	 * @return 	true if roll1 higher than roll2, false otherwise. 
 	 */
 	public static boolean isHigher(double roll1, double roll2) {
 		if(roll1 > roll2) {
@@ -34,6 +34,46 @@ public class ElectionProtocol {
 			return false; 
 		} 
 		return true; 
+	}
+	
+	public static void main(String args[]) {
+		//All servers do their initial roll on their individual systems 
+		double server1 = roll(); 
+		double server2 = roll(); 
+		double server3 = roll(); 
+		
+		String coord = "Server0"; 
+		//Usage example 
+		//Need to translate to servers communicating with one another
+		
+		//Server 1 challenges server 2, server 2 receives "initial"(in decoder) message
+		//server 2 sends back roll 
+		//Server 1 runs isHigher() and compares 
+		//If Server 1 is higher, then compare server 1 to server 3 
+		//If Server 2 is higher, then compare server 2 to server 3 
+		//Server x challenges server 3, server 3 receives "initial"(in decoder) message 
+		//server 3 sends back roll 
+		//Server x runs isHigher() and compares 
+		//If Server x is higher, then server x is the coordinator 
+		//If Server 3 is higher, then server 3 is the coordinator 
+		if(isHigher(server1,server2)) {
+			
+			if(isHigher(server1, server3)) { 
+				coord = "Server1"; 
+			} else {
+				coord = "Server3"; 
+			}
+		} else {
+			if(isHigher(server2,server3)) {
+				coord = "Server2"; 
+			} else {
+				coord = "Server3";
+			}
+		}
+		
+		System.out.println(coord); 
+		
+		
 	}
 
 }
