23f70c143b924b36a248fa2284e4e8b7e537d2e7
Jocelyn <36179505+jjedd97@users.noreply.github.com>
2020-04-11 11:26:36 -0600

    Bug fix+ untested send

--- src/main/java/server/Store.java
+++ src/main/java/server/Store.java
@@ -1,15 +1,22 @@
 package server;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.nio.file.Files;
 
 public class Store {
 	
@@ -27,9 +34,6 @@ public class Store {
 			out = new RandomAccessFile("output.txt", "rw");
 		}
 		 catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -52,29 +56,97 @@ public class Store {
 		
 	    public void writeLast(String last) {
 			synchronized(lock) {
+				BufferedReader reader=null;
 				try { 
-					BufferedReader reader = new BufferedReader(new InputStreamReader( new FileInputStream(f), "UTF-8"));
+					reader = new BufferedReader(new InputStreamReader( new FileInputStream(f), "UTF-8"));
 					String line = reader.readLine();
+					reader.close();
 					if (line.length()>last.length()){
 						throw new IllegalArgumentException("Last must not be a shorter line");
 					}
 					out.seek(0);
-				    out.write(last.getBytes());
+				    updateLine(line, last);
+				    
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				  catch( NullPointerException e){
 					  try {
-							out.seek(0);
 							out.write(last.getBytes());
 						} catch (IOException ee) {
 							// TODO Auto-generated catch block
 							ee.printStackTrace();
 						}
 					  
-				  }
+				  } finally {
+				        if (reader != null)
+							try {
+								reader.close();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+							}
+				    }
 
 			}
 	    }
+	    
+	    private void updateLine(String toUpdate, String updated) throws IOException {
+	        BufferedReader file = new BufferedReader(new FileReader(f));
+	        File temp=new File(f+".out");
+	        PrintWriter writer = new PrintWriter(temp, "UTF-8");
+	        String line;
+
+	        while ((line = file.readLine()) != null)
+	        {
+	            line = line.replace(toUpdate, updated);
+	            writer.println(line);
+	        }
+	        file.close();
+	        writer.close();
+	        out.close();
+	        System.out.println("hello");
+	        Files.move(temp.getAbsoluteFile().toPath(), f.getAbsoluteFile().toPath(), REPLACE_EXISTING);     
+	        out = new RandomAccessFile("output.txt", "rw");
+	        System.out.println("test");
+	    }
+	    
+	   public void send(Socket s) {
+		   synchronized(lock) {
+		          byte [] mybytearray  = new byte [(int)f.length()];
+		          FileInputStream fis;
+				try {
+					fis = new FileInputStream(f);
+					BufferedInputStream bis = new BufferedInputStream(fis);
+			        bis.read(mybytearray,0,mybytearray.length);
+			        OutputStream os = s.getOutputStream();
+			        System.out.println("Sending " + f + "(" + mybytearray.length + " bytes)");
+			        os.write(mybytearray,0,mybytearray.length);
+			        os.flush();
+			        System.out.println(f+" sent");
+			        if (fis != null) bis.close();
+			        if (bis != null) bis.close();
+			        if (os != null) os.close();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		   }
+	   }
+	    
+	   public void shutdown() {
+		   synchronized(lock) {
+			 try {
+				out.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}  
+			   
+		   }
+		   
+	   }
 }
--- src/test/java/server/StorageTest.java
+++ src/test/java/server/StorageTest.java
@@ -17,21 +17,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StorageTest {
-	
+
 	@Test
 	void empty() {
 		BufferedReader reader;
 		try {
-			//empty file
-			new PrintWriter("output.txt").close();
-			//
-			File f=new File("output.txt");
-			reader = new BufferedReader(new InputStreamReader( new FileInputStream(f), "UTF-8"));
 			Store s=new Store();
 	        String w="Lastchecked:10";
 			s.writeLast(w);
+			File f=new File("output.txt");
+			reader = new BufferedReader(new InputStreamReader( new FileInputStream(f), "UTF-8"));
 			String line = reader.readLine();
 			assertTrue(line.equals(w));
+			reader.close();
+			s.shutdown();
 		} catch (UnsupportedEncodingException | FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -44,15 +43,17 @@ public class StorageTest {
 	void replaceLonger() {
 		BufferedReader reader;
 		try {
-			File f=new File("output.txt");
-			reader = new BufferedReader(new InputStreamReader( new FileInputStream(f), "UTF-8"));
 			Store s=new Store();
 	        String one="Lastchecked:10";
 	        String two="Lastchecked:1111";
 			s.writeLast(one);
 			s.writeLast(two);
+			File f=new File("output.txt");
+			reader = new BufferedReader(new InputStreamReader( new FileInputStream(f), "UTF-8"));
 			String line = reader.readLine();
 			assertTrue(line.equals(two));
+			reader.close();
+			s.shutdown();
 		} catch (UnsupportedEncodingException | FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -65,9 +66,8 @@ public class StorageTest {
 	
 	@Test
 	void replaceShorter() {
-		BufferedReader reader;
+		BufferedReader reader=null;
 		try {
-			
 			File f=new File("output.txt");
 			reader = new BufferedReader(new InputStreamReader( new FileInputStream(f), "UTF-8"));
 			Store s=new Store();
@@ -76,12 +76,20 @@ public class StorageTest {
 			s.writeLast(two);
 			assertThrows(IllegalArgumentException.class, 
 				 ()->s.writeLast(one));
+			s.shutdown();
 		} catch (UnsupportedEncodingException | FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			if (reader !=null) {try {
+				reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
 		}
 	}
 	 
@@ -99,6 +107,8 @@ public class StorageTest {
 			String line = reader.readLine();
 			line=reader.readLine();
 			assertTrue(line.equals("Prime:17"));
+			reader.close();
+			s.shutdown();
 		} catch (UnsupportedEncodingException | FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -107,4 +117,33 @@ public class StorageTest {
 			e.printStackTrace();
 		}
 	} 
+	
+	@Test
+	void sandwhich() {
+		BufferedReader reader;
+		try {
+			
+			Store s=new Store();
+	        String w="Lastchecked:10";
+			s.writeLast(w);
+			s.writeResult("Prime:17");
+			s.writeLast("Lastchecked:1111");
+			File f=new File("output.txt");
+			reader = new BufferedReader(new InputStreamReader( new FileInputStream(f), "UTF-8"));
+			String line = reader.readLine();
+			line=reader.readLine();
+			assertTrue(line.equals("Prime:17"));
+			reader.close();
+			s.shutdown();
+		} catch (UnsupportedEncodingException | FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	} 
+	 
+	
+	
 }
