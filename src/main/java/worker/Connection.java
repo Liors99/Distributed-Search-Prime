24a6eacb0b3b24c76b180ddeaef915accdfe9e19
mark.german <mark.german@ucalgary.ca>
2020-04-12 13:25:44 -0600

    adding basic worker networking

---
+++ src/main/java/worker/Connection.java
@@ -0,0 +1,33 @@
+package worker;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.*;
+
+public class Connection extends Thread{
+
+	private String hostname;
+	private int port;
+	private Socket sock;
+	private InputStream sockIn;
+	private OutputStream sockOut;
+	
+	public Connection(String hostname, int port) {
+		this.hostname = hostname;
+		this.port = port;
+		
+	}
+	
+	
+	public void connect() {
+		try {
+			sock = new Socket(hostname, port);
+			sockIn = sock.getInputStream();
+			sockOut = sock.getOutputStream();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/worker/Networking.java
@@ -0,0 +1,39 @@
+package worker;
+import java.net.*;
+
+public class Networking {
+	public static final int NUMBER_OF_SERVERS = 3;
+	
+	private class ConnectionInfo{
+		String hostname;
+		int port;
+		
+		public ConnectionInfo() {
+			hostname = "localhost";
+			port = 8080;
+		}
+		
+		public ConnectionInfo(String hostname, int port) {
+			this.hostname = hostname;
+			this.port = port;
+		}
+	}
+	
+	private ConnectionInfo[] connections;
+	
+	public Networking() {
+		connections = new ConnectionInfo[3];
+		for (int i = 0; i<NUMBER_OF_SERVERS; i++) {
+			connections[i] = new ConnectionInfo();
+		}
+		
+	}
+	
+	
+	public void registerServers(String[] hostnames, int[] ports) {
+		for (int i = 0; i<NUMBER_OF_SERVERS; i++) {
+			connections[i] = new ConnectionInfo(hostnames[i], ports[i]);			
+		}
+	}
+
+}
